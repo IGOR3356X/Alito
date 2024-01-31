b7a6f7cd0ae2beda5afc8605752c3a38dfdc2d14
DanVoron <danvoron3@gmail.com>
2024-01-31 11:30:16 +0300

    NACHALO

---
+++ Classes/Entities/Exam.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alito.Classes.Entities
+{
+    internal class Exam
+    {
+        public string Questions;
+
+        public string HandRightAnswer;
+
+        public string RightAnswer;
+
+        public int ExamAffectionUP;
+    }
+}
---
+++ Classes/Entities/Subject.cs
@@ -0,0 +1,24 @@
+﻿using Alito.Classes.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Controls;
+
+namespace Alito.Class
+{
+    internal class Subject
+    {
+        public class OGE
+        {
+            public string TasksOGE;
+        }
+
+        public class EGE
+        {
+            public string TasksEGE;
+        }
+    }
+}
